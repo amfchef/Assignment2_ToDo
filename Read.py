c5a109acbc977100a557663caf60023d98a4d0d1
Jakob Johansson <jakobtj@hotmail.com>
2020-12-09 20:53:09 +0100

    Second commit, classes added

---
+++ Read.py
@@ -0,0 +1,12 @@
+class Read():
+
+    def read_file(self):
+        name_of_file = (input("Please enter the name of your existing "
+                              "file you would like to read from: "))
+        name_of_file = name_of_file + ".txt"
+        try:
+            my_file = open(name_of_file, "a+")
+            file_input = (input("Please enter your input in your file: "))
+            my_file.write(file_input)
+        except OSError as e:
+            print("Error!\nThe file was not found")
